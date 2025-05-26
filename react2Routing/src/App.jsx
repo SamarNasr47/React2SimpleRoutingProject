8643180fc49f968564e96fd9dc36f28eefb6b241
SamarNasr47 <samarhashem2001@gmail.com>
2025-05-26 21:01:59 +0300

    initial commit

--- react2Routing/src/App.jsx
+++ react2Routing/src/App.jsx
@@ -4,30 +4,32 @@ import Home from './pages/Home/Home.jsx'
 import Settings from './pages/Settings/Settings.jsx'
 import Users from './pages/Users/Users.jsx'
 import Logout from './pages/Logout/Logout.jsx'
-import { Link, Outlet } from 'react-router-dom'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-
-const routes = createBrowserRouter([
+const routes = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <SideMenu />,
+      children: [
+        { path: "Home", element: <Home /> },
+        { path: "Settings", element: <Settings /> },
+        { path: "Users", element: <Users /> },
+        { path: "Logout", element: <Logout /> },
+        { path: "*", element: <h1>404 - Page Not Found</h1> },
+      ],
+    },
+  ],
   {
-    path: "", element: <SideMenu />,
-    children: [
-      { path: "/Home", element: <Home /> },
-      { path: "/Settings", element: <Settings /> },
-      { path: "/Users", element: <Users /> },
-      { path: "/Logout", element: <Logout /> }
-    ],
+    basename: "/React2SimpleRoutingProject",
   }
-])
+)
 
 function App() {
-
   return (
-    <>
-      <div className="flex gap-40">
-        <RouterProvider router={routes} />
-      </div>
-    </>
+    <div className="flex gap-40">
+      <RouterProvider router={routes} />
+    </div>
   )
 }
 
